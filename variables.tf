472b4590ccd4a5f1f1712fefe6cd329f6558480b
Andrii Guselietov <guseletov@gmail.com>
2020-11-11 15:35:30 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+# TFE
+variable "organization_name" {
+  description = "Name of the organization in which this workspace should be created"
+}
+
+variable "workspace_name" {
+  description = "The name of the workspace to be created"
+}
